4fb8a118abf7191f7b5e22137f9beefed8d25434
Descripiface <61946248+Descripiface@users.noreply.github.com>
2021-12-23 13:56:18 +0300

    Add files via upload

---
+++ b_threadedserv/clientHw.py
@@ -0,0 +1,29 @@
+import socket
+
+PORT = 9090
+ADDR = ('localhost', PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
+
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('localhost', 9090))
+    print(f"[CONNECTED] Client connected to server at port {PORT}")
+
+    connected = True
+    while connected:
+        msg = input("> ")
+
+        client.send(msg.encode(FORMAT))
+
+        if msg == DISCONNECT_MSG:
+            connected = False
+        else:
+            msg = client.recv(SIZE).decode(FORMAT)
+            print(f"[SERVER] {msg}")
+
+
+if __name__ == "__main__":
+    main()
